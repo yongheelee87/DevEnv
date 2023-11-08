0fca65f0c7d1412667422cb791f10dca79a9031e
YONG\yongh <yongheelee87@gmail.com>
2023-11-08 13:02:26 +0900

    [SWVERIFY] SW결과 HTML 수정 [Function] 프로젝트 [Description] SW결과 HTML 수정

--- SwVerification/Lib/Common/buildDisplay.py
+++ SwVerification/Lib/Common/buildDisplay.py
@@ -49,6 +49,8 @@ def make_pjt_HTML(df_sum, project: str, dict_tc: dict, export_path: str):
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
             <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap' type='text/css'>
+            <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Jua&display=swap' type='text/css'>
+            <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Nunito:400,300' type='text/css'>
             <title>{title} Result</title>
         </head>
         <body>
@@ -69,12 +71,11 @@ def make_home_HTML(data: dict, export_path: str):
     test_date = os.path.basename(export_path)
 
     lst_table = [
-        '<table style="border: none;border-collapse: collapse;font-size: 1.0em;color: black;margin-bottom: 40px;margin-left: 20px;padding: 20px;">',
-        '<tr style="background-color: #ECF0F1;border: 1px solid #54585d;">',
-        '<th style="padding: 15px;border: 1px solid #54585d;text-align: center;">모듈</th>',
-        '<th style="padding: 15px;border: 1px solid #54585d;text-align: center;">기능</th>',
-        '<th style="padding: 15px;border: 1px solid #54585d;text-align: center;">결과</th>',
-        '<th style="padding: 15px;border: 1px solid #54585d;text-align: center;">상세 링크</th>',
+        '<tr style="background-color: #54585d;border: 1px solid #54585d;">',
+        '<th style="padding: 15px;border-right: 1px solid #54585d;text-align: center;color: white;">모듈</th>',
+        '<th style="padding: 15px;border-right: 1px solid #54585d;text-align: center;color: white;">기능</th>',
+        '<th style="padding: 15px;border-right: 1px solid #54585d;text-align: center;color: white;">결과</th>',
+        '<th style="padding: 15px;text-align: center;color: white;">상세 링크</th>',
         '</tr>']
 
     for pjt in data.keys():
@@ -97,7 +98,6 @@ def make_home_HTML(data: dict, export_path: str):
                 lst_table.append('<td rowspan={len_row} style="padding: 15px;border: 1px solid #54585d;text-align: center"><a href="{project}/Result_{project}.html">{project}<br>{date}</a></td>'.format(len_row=len(pjt_data), project=pjt, date=test_date))
                 module_tr_written = True
             lst_table.append('</tr>')
-    lst_table.append('</table>')
     sum_result = '\n'.join(lst_table)
 
     html_text = """
@@ -108,12 +108,16 @@ def make_home_HTML(data: dict, export_path: str):
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
             <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap' type='text/css'>
+            <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Jua&display=swap' type='text/css'>
+            <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Nunito:400,300' type='text/css'>
             <title>{title} Result</title>
         </head>
         <body>
             <h1 style="font-family: 'Black Han Sans', sans-serif;font-size: 2.5em;color: red;margin: 20px 0 10px 10px;>{title}</h1>
-            <h2 style="font-family: 'Black Han Sans', sans-serif;font-size: 1.0em;color: red;margin: 20px 0 10px 10px;>테스트 수행 날짜: {date}</h2>
-    {sum_body}
+            <h2 style="font-family: 'Jua', sans-serif;font-size: 1.0em;color: red;margin: 20px 0 10px 10px;>테스트 수행 날짜: {date}</h2>
+            <table style="font-family: 'Nunito', sans-serif;border: none;border-collapse: collapse;font-size: 1.0em;color: black;margin-bottom: 40px;margin-left: 20px;padding: 20px;">
+            {sum_body}
+            </table>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         </body>
     </html>
@@ -141,7 +145,7 @@ def _write_summary(df_sum):
             else:
                 line = line.replace('<td>', '<td style="text-align: right;padding-left: 30px;">')
         elif 'class=dataframe' in line:
-            line = """<table class="dataframe" style="border: none;border-collapse: collapse;font-size: 1.0em;color: black;margin-bottom: 40px;margin-left: 20px;padding: 20px;">"""
+            line = """<table class="dataframe" style="font-family: 'Nunito', sans-serif;border: none;border-collapse: collapse;font-size: 1.0em;color: black;margin-bottom: 40px;margin-left: 20px;padding: 20px;">"""
         new_html.append(line)
     sum_html = '\n'.join(new_html)
     return sum_html
@@ -150,8 +154,8 @@ def _write_img_body(dict_tc: dict):
     img_html = ''
     for lmg_src in dict_tc.keys():
         img_body = """
-            <h3 style="font-family: 'Black Han Sans', sans-serif;font-size: 1em;color: red;margin: 0 0 0 10px;>{sub_title}</h3>
-            <img src="{img_src}" width="1100" height="1100" style="width: 1100px; height: 880px; object-fit:cover;margin: 0 0 20px 0px;" alt="NOT FOUND"></img>
+            <h3 style="font-family: 'Jua', sans-serif;font-size: 1em;color: red;margin: 0 0 0 10px;>{sub_title}</h3>
+            <img src="{img_src}" width="1200" height="1200" style="width: 1200px; height: 950px; object-fit:cover;margin: 0 0 40px 0px;" alt="NOT FOUND"></img>
         """
         img_html += img_body.format(sub_title=dict_tc[lmg_src], img_src=lmg_src + '.png')
     return img_html
